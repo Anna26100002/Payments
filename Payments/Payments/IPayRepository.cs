e0c0514912dda5cd8d465bad2e0deb87ef2585d7
Anna26100002 <69114344+Anna26100002@users.noreply.github.com>
2022-04-24 23:32:28 +0300

    Add files via upload

---
+++ Payments/Payments/IPayRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payments
+{
+    public interface IPayRepository
+    {
+        Cash InsertCash(Cash cash); //Добавление прихода денег
+        Orders InsertOrder(Orders order); //Добавление заказа
+        Payment InsertPayment(Payment payment); //Добавление платежа
+        IEnumerable<Payment> SelectPayment(); //Список платежей
+        IEnumerable<Cash> SelectCash(); //Список прихода денег
+        IEnumerable<Orders> SelectOrder(); //Список заказов
+        int GetCash(int number); //Выбор 1 прихода денег
+        int GetOrder(int orderNumber); //Выбор 1 заказа
+    }
+}
