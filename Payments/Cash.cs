868595805dcf719189cc8e033d419011ebb825d4
Anna26100002 <a.p.parshikova@gmail.com>
2022-04-24 23:13:06 +0300

    Dapper

---
+++ Payments/Cash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payments
+{
+    public class Cash
+    {
+        public int Number { get; set; }
+        public DateTime TheDate { get; set; }
+        public int Amount { get; set; }
+        public int Remainder { get; set; }
+    }
+}
---
+++ Payments/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payments
+{
+    public class Orders
+    {
+        public int OrderNumber { get; set; }
+        public DateTime TheDate { get; set; }
+        public int Amount { get; set; }
+        public int PaymentAmount { get; set; }
+    }
+}
